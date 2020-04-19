0c979ae774f3ba834b62338ca43b10c1d480e4e2
Leonardo Matos <leomp120894@gmail.com>
2020-04-18 21:03:55 -0300

    chore(public): setup onload expression script

---
+++ functions/public/onload-expression.js
@@ -0,0 +1,11 @@
+;(function () {
+  window._mpHash = function () {
+    return new Promise(function (resolve, reject) {
+    })
+  }
+
+  window._mpInstallments = function () {
+    return new Promise(function (resolve, reject) {
+    })
+  }
+}())
