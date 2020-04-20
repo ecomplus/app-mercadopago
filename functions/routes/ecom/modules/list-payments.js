8de57995b4daf14ccb730adcc9e5882d92585e4d
Leonardo Matos <leomp120894@gmail.com>
2020-04-20 13:21:51 -0300

    fix(list-payments): fix reading onload expression minified file

--- functions/routes/ecom/modules/list-payments.js
+++ functions/routes/ecom/modules/list-payments.js
@@ -1,23 +1,21 @@
-const newPaymentGatways = require('./../../../lib/new-payment-gateway')
+const path = require('path')
+const fs = require('fs')
 
 exports.post = ({ appSdk }, req, res) => {
   // body was already pre-validated on @/bin/web.js
   // treat module request body
   const { params, application } = req.body
+  const amount = params.amount || {}
 
   // app configured options
   const config = Object.assign({}, application.data, application.hidden_data)
-  const mpCheckoutLabel = config.label || 'Pagar com Mercado Pago'
-
   if (!config.mp_public_key || !config.mp_access_token) {
     // must have configured PayPal app ID and secret
     return res.status(400).send({
       error: 'LIST_PAYMENTS_ERR',
-      message: 'Mercado pago Public Key or Access Token is unset on app hidden data (merchant must configure the app)'
+      message: 'MP Public Key or Access Token is unset on app hidden data (merchant must configure the app)'
     })
   }
-
-  const amount = params.amount || {}
 
   // start mounting response body
   // https://apx-mods.e-com.plus/api/v1/list_payments/response_schema.json?store_id=100
@@ -35,12 +33,12 @@ exports.post = ({ appSdk }, req, res) => {
         label: config.discount_option_label,
         value
       }
-        // specify the discount type and min amount is optional
-        ;['type', 'min_amount'].forEach(prop => {
-          if (discount[prop]) {
-            response.discount_option[prop] = discount[prop]
-          }
-        })
+      // specify the discount type and min amount is optional
+      ;['type', 'min_amount'].forEach(prop => {
+        if (discount[prop]) {
+          response.discount_option[prop] = discount[prop]
+        }
+      })
     }
 
     if (amount.total) {
@@ -72,11 +70,47 @@ exports.post = ({ appSdk }, req, res) => {
     }
   }
 
-  const paymentGateway = newPaymentGatways()
-  const onloadFunction = `window.Mercadopago.setPublishableKey("${config.mp_public_key}");`
-  paymentGateway.js_client.onload_expression = onloadFunction + paymentGateway.js_client.onload_expression
-  paymentGateway.label = mpCheckoutLabel
-  paymentGateway.payment_method.name = mpCheckoutLabel
+  // setup default payment gateway object
+  const label = config.label || 'Cartão de crédito'
+  const paymentGateway = {
+    label,
+    payment_method: {
+      code: 'credit_card',
+      name: `${label} - Mercado Pago`
+    },
+    intermediator: {
+      code: 'mercadopago',
+      link: 'https://www.mercadopago.com.br',
+      name: 'Mercado Pago'
+    },
+    payment_url: 'https://www.mercadopago.com.br/',
+    type: 'payment',
+    js_client: {
+      script_uri: 'https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js',
+      onload_expression: `window.Mercadopago.setPublishableKey("${config.mp_public_key}");` +
+        fs.readFileSync(path.join(__dirname, '../../../public/onload-expression.min.js'), 'utf8'),
+      cc_brand: {
+        function: '_mpBrand',
+        is_promise: true
+      },
+      cc_hash: {
+        function: '_mpHash',
+        is_promise: true
+      },
+      cc_installments: {
+        function: '_mpInstallments',
+        is_promise: true
+      }
+    }
+  }
+
+  // additional payment gateway config
+  if (config.text) {
+    paymentGateway.text = config.text
+  }
+  if (config.icon) {
+    paymentGateway.icon = config.icon
+  }
 
   // check available discount by payment method
   if (discount && discount.value > 0) {
@@ -86,22 +120,10 @@ exports.post = ({ appSdk }, req, res) => {
     }
   }
 
+  // default configured installments option
   const installmentsOption = config.installments_option
   if (installmentsOption && installmentsOption.max_number) {
-    // default configured installments option
     response.installments_option = installmentsOption
-  }
-
-  if (config.text) {
-    paymentGateway.text = config.text
-  }
-
-  if (config.icon) {
-    paymentGateway.icon = config.icon
-  }
-
-  if (!config.disable_credit_card) {
-    response.payment_gateways.unshift(paymentGateway)
   }
 
   res.send(response)
