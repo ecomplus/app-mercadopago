a54a6ee463c6cc5a0f38d27a51572868b5dd8b82
Leonardo Matos <leomp120894@gmail.com>
2021-02-05 11:45:31 -0300

    chore(update-tokens): edit crontab to run each 4 hours

--- functions/index.js
+++ functions/index.js
@@ -133,7 +133,7 @@ exports[functionName] = functions.https.onRequest(server)
 console.log(`-- Starting '${app.title}' E-Com Plus app with Function '${functionName}'`)
 
 // schedule update tokens job
-const cron = '36 1,6,12,18 * * *'
+const cron = '48 */4 * * *'
 exports.updateTokens = functions.pubsub.schedule(cron).onRun(() => {
   return prepareAppSdk().then(appSdk => {
     return appSdk.updateTokens()
